c20da8ba4e12bf21282d1e0fa97778eea10cb164
Juanin <juanc.23.g@ciencias.unam.mx>
2019-09-08 19:40:30 -0500

    practica 3

--- PersonalBlog/Home/views.py
+++ PersonalBlog/Home/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 def index(request):
     context = {}
     return render(request, 'Home/index.html', context)
+
+# Class-based Views
+class Index(View):
+    """
+        Index in my Web Page but with Clased based views.
+    """
+    template = 'Home/index.html'
+    context = {'title': 'Index'}
+
+    def get(self, request):
+        """
+            Get in my Index.
+        """
+        print('With class-based views')
+        return render(request, self.template, self.context)
+
+
+class About(View):
+    """
+        About me page.
+    """
+    template = 'Home/about.html'
+    context = {'title': 'About me'}
+
+    def get(self, request):
+        """
+            Get in About me.
+        """
+        return render(request, self.template, self.context)
